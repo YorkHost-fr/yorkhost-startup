2d91e703e0221a1c57ffe682ac66df61ff76636e
Arturo DERGAL - YorkHost <144963931+arturod67@users.noreply.github.com>
2026-02-23 20:44:32 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 if [ ! -x "$(pwd)/alpine/opt/cfx-server/FXServer" ]; then
   echo "** Alpine missing or broken, reinstalling artifacts..."
   rm -rf alpine
-  DOWNLOAD_LINK=$(curl -sSL https://changelogs-live.fivem.net/api/changelog/versions/linux/server | grep -o '"latest_download":"[^"]*"' | cut -d'"' -f4)
+  DOWNLOAD_LINK=$(curl -sSL https://changelogs-live.fivem.net/api/changelog/versions/linux/server | grep -o '"recommended_download":"[^"]*"' | cut -d'"' -f4)
   echo "Downloading: ${DOWNLOAD_LINK}"
   curl -sSL "${DOWNLOAD_LINK}" -o fx.tar.xz
   tar xf fx.tar.xz
